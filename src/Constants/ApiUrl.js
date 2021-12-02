c399eb2aac1433435f6862594151c62f97e7ee47
kadgayesameer <kadgayesameer@gmail.com>
2021-12-02 16:52:36 +0530

    First Commit

--- src/Constants/ApiUrl.js
+++ src/Constants/ApiUrl.js
@@ -1,7 +1,7 @@
-// export const API_URL = 'http://192.168.5.151:7878'
-// export const ApiUrl = 'http://192.168.5.151:7878'
-export const API_URL = 'http://localhost:7878'
+export const API_URL = 'http://192.168.5.151:7878'
+export const ApiUrl = 'http://192.168.5.151:7878'
+// export const API_URL = 'http://localhost:7878'
 // export const ApiUrl = 'http://localhost:7878'
-export const ApiUrl = 'http://localhost:7878'
+// export const ApiUrl = 'http://localhost:7878'
 
 // export const ApiUrl = 'http://localhost:9595'
